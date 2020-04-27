8daf6c9809ef4cc73911b9cfd23ed88aa7f7b0a9
molch419 <molch419@school.lu>
2020-04-27 17:27:48 +0200

    spaces have been reduced

--- login.php
+++ login.php
@@ -8,12 +8,8 @@ include_once "displayUser.php";
 
 if (isset($_POST["Logout"])) {
 
-
-
   session_unset();
-
   session_destroy();
-
   print "You have been successfully logged-out" . "<br>";
 
   ?>
@@ -25,41 +21,25 @@ if (isset($_POST["Logout"])) {
 } elseif ($_SESSION["UserLogged"]) {
 
   print "You have already been logged-in" . "<br>";
-
   displayUserDetails($connection);
-
 } elseif (isset($_POST["Username"]) && isset($_POST["Password"])) {
-
   $userFromMyDatabase = $connection->prepare(
-
     "SELECT * FROM ppl WHERE UserName=?"
-
   );
-
   $userFromMyDatabase->bind_param("s", $_POST["Username"]);
-
   $userFromMyDatabase->execute();
-
   $result = $userFromMyDatabase->get_result();
-
   if ($result->num_rows === 1) {
 
     print "Your password is being verified " . "<br>";
-
     $row = $result->fetch_assoc();
-
     if (password_verify($_POST["Password"], $row["Password"])) {
 
       $_SESSION["UserLogged"] = true;
-
       $_SESSION["CurrentUser"] = $row["PERSON_ID"];
-
       displayUserDetails($connection);
-
     } else {
-
       print "Password mismatched ! Please type your password correctly";
-
     }
 
   } else {
@@ -67,25 +47,16 @@ if (isset($_POST["Logout"])) {
     print "The username you typed has not been found in our database !!"; ?>
 
     <a href="signup.php">Please register first</a> <br>
-
     <a href="login.php">Try again to login</a>
-
     <?php
-
   }
-
 } else {
 
    ?>
-
     <form action="login.php" method="post">
-
         Username: <input type="text" name="Username" placeholder="Username" required><br>
-
         Password: <input type="password" name="Password" placeholder="Password" required><br>
-
         <input type="submit" name="Login" value="Login">
-
     </form>
 
 <?php
